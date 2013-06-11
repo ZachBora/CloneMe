50b9815baf30d4b16dd8312afad137e04fc0e5ad
Zach Bora <zachbora@gmail.com>
2013-06-10 20:54:48 -0400

    0.12

---
+++ src/com/worldcretornica/cloneme/compat/v1_5_R3/NPC.java
@@ -0,0 +1,31 @@
+package com.worldcretornica.cloneme.compat.v1_5_R3;
+
+import net.minecraft.server.v1_5_R3.*;
+import org.bukkit.craftbukkit.v1_5_R3.entity.*;
+
+import org.bukkit.Location;
+import org.bukkit.entity.HumanEntity;
+
+import com.worldcretornica.cloneme.compat.INPC;
+
+public class NPC implements INPC 
+{
+	@Override
+	public void setYaw(HumanEntity entity, Location loc) 
+	{
+		((CraftHumanEntity) entity).getHandle().aA = loc.getYaw();
+	}
+
+	@Override
+	public void armSwing(HumanEntity entity) 
+	{
+		EntityHuman eh = ((CraftHumanEntity) entity).getHandle();
+		((WorldServer) eh.world).tracker.a(eh, new Packet18ArmAnimation(eh, 1));
+	}
+
+	@Override
+	public void setSneak(HumanEntity entity, boolean sneak) 
+	{
+		((CraftHumanEntity) entity).getHandle().setSneaking(sneak);
+	}
+}
